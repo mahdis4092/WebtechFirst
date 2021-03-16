e0a90a466cc3ace035e78a81074d2b8ea2ba2e15
mahdis4092 <77955640+mahdis4092@users.noreply.github.com>
2021-03-17 01:40:48 +0600

    Add files via upload
    Lab task 4

---
+++ Lab task4/Home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <style >
+  header {
+  background-color: #b2beb5;
+  padding: 20px;
+  text-align: right;
+  font-size: 20px;
+  color: green;
+
+}
+  footer {
+  background-color: #777;
+  padding: 20px;
+  text-align: center;
+  color: white;
+}
+
+
+  </style>
+</head>
+<body>
+
+<header>
+  <h2>X company</h2>
+  <h1 ></h1>
+
+  <div class="topnav">
+  <a  href="Home.html" > Home </a>
+  <a href="Login.php">Login </a>
+  <a href="Store.php">Registration </a>
+
+</div>
+</header>
+
+<h3 ><font size="20px">  Welcome to X company  </font></h3>
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+<footer>
+  <p>Copyright <span>&#169;</span>Mahdi Hasan Shuvo</p>
+</footer>
+
+</body>
+</html>
